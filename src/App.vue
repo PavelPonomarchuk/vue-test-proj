c091088a0c9199c40e0e6c2c2032db2e07a8888e
PavelPonomarchuk <ponomarchuk83@gmail.com>
2020-07-29 14:28:28 +0300

    Первый коммит всплывашка с ошибкой

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Какая-то страница</h1>
+    <hr>
+    <TodoList />
+    <UnknownError />
+
+
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import UnknownError from '@/components/UnknownError'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    UnknownError
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
